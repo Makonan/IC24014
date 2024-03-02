122a7c345aab9cab0bba2ac0471748250b38011f
Makonan <161947267+Makonan@users.noreply.github.com>
2024-03-02 08:32:37 -0500

    Add files via upload

---
+++ IC24 TEAM14/VerticalApps Tool Project/questions.js
@@ -0,0 +1,59 @@
+let questions = [
+  {
+    numb: 1,
+    question: "How do you perceive illegal immigration?",
+    answer: "A. Illegal Immigration is simply Illegal",
+
+    options: [
+      "A. Illegal Immigration is simply Illegal",
+      "B. It's a humanitarian concern and we need inclusive policies",
+      "C. It will ruin our country. We are better than that",
+      "D. Unauthorized migrants provide a boost to our economic growth",
+    ],
+  },
+  {
+    numb: 2,
+    question: "More economic growth or more social benefits ?",
+    answer: "B. We should all have access to healthcare, education and jobs",
+    options: [
+      "A. Economy doesn't feed us. We feed economy. The people first. ",
+      "B. We should all have access to healthcare, education and jobs",
+      "C. We need our economy back. China is going too far. Too far",
+      "D. Let's finnish what we started. Save Ukraine !",
+    ],
+  },
+  {
+    numb: 3,
+    question: "We need stricter gun control policies or not because",
+    answer: "C. Every individual deserves to live in safety and peace",
+    options: [
+      "A. It perpetuates fear and division",
+      "B. I will kill the target before the target kills me",
+      "C. Every individual deserves to live in safety and peace",
+      "D. I want to buy a gun",
+    ],
+  },
+  {
+    numb: 4,
+    question: "What are your thoughts on climate change?",
+    answer:
+      "B. Yes, climate change is a global issue that requires coordinated efforts from all nations",
+    options: [
+      "A. No, the costs of implementing solutions to climate change is too much",
+      "B. Yes, climate change is a global issue that requires coordinated efforts from all nations",
+      "C. Even if climate chage exists. We'll die anyways",
+      "D. Yes, the U.S. is responsible",
+    ],
+  },
+  {
+    numb: 5,
+    question: "Why is the sky blue?",
+    answer: "C. The sky is blue because it took its mother's eyes",
+    options: [
+      "A. The sky is blue because if it was yellow, it would be called sunny",
+      "B. The sky has no color",
+      "C. The sky is blue because it took its mother's eyes",
+      "D. The sky is not blue",
+    ],
+  },
+];
